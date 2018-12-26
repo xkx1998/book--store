22e0cabe7854f9ab7d6cfbc41c6eb40cf2bf5b32
xkx1998 <2179007293@qq.com>
2018-12-26 23:31:05 +0800

    增加sql文件

---
+++ bookstore/WebRoot/sql/BOOK_data.sql
@@ -0,0 +1,16 @@
+REM INSERTING into BOOK
+Insert into BOOK (BID,BNAME,PRICE,AUTHOR,IMAGE,CID) values ('1','深入理解java虚拟机',12.2,'14.00','book_img/20.jpg',1);
+Insert into BOOK (BID,BNAME,PRICE,AUTHOR,IMAGE,CID) values ('19','spring boot',50.6,'某某','book_img/223B7345D8A24F6D93A044634377FB98_23926195-1_b_11.jpg',4);
+Insert into BOOK (BID,BNAME,PRICE,AUTHOR,IMAGE,CID) values ('3','精通spring',33.6,'郑阿奇','book_img/20029394-1_l.jpg',2);
+Insert into BOOK (BID,BNAME,PRICE,AUTHOR,IMAGE,CID) values ('4','javaweb开发详解',44.4,'明日科技','book_img/22788412-1_l.jpg',2);
+Insert into BOOK (BID,BNAME,PRICE,AUTHOR,IMAGE,CID) values ('5','JAVA疯狂讲义',66.1,'信息科学','book_img/23532609-1_b_4.jpg',1);
+Insert into BOOK (BID,BNAME,PRICE,AUTHOR,IMAGE,CID) values ('13','spring',100,'科比','book_img/F8445F5418424E0AA41CBCE506B4B880_20029394-1_l.jpg',4);
+Insert into BOOK (BID,BNAME,PRICE,AUTHOR,IMAGE,CID) values ('19','java 核心技术',150,'马飞','book_img/FC65FEA30D6D42A682DB5DC4759D7491_27.jpg',1);
+Insert into BOOK (BID,BNAME,PRICE,AUTHOR,IMAGE,CID) values ('6','java 编程思想',100,'迈克尔·丢雷·楼某','book_img/C8CB0C0F1B144ED496C384AE220CCFF5_9317290-1_l.jpg',4);
+Insert into BOOK (BID,BNAME,PRICE,AUTHOR,IMAGE,CID) values ('19','hibernate',100,'xkx','book_img/1E9B3654546A46638468B816C407553A_8991366-1_l.jpg',4);
+Insert into BOOK (BID,BNAME,PRICE,AUTHOR,IMAGE,CID) values ('11','struts 2',111,'xx','book_img/453F37E71C984215BB15CE94D3A91EA0_20385925-1_l.jpg',4);
+Insert into BOOK (BID,BNAME,PRICE,AUTHOR,IMAGE,CID) values ('5','SSM',106,'diulei','book_img/4F33126D292D47A2B554083E6DCF4448_25111311-1_b_2.jpg',4);
+Insert into BOOK (BID,BNAME,PRICE,AUTHOR,IMAGE,CID) values ('14','java 设计模式',102,'不知道','book_img/42BCEB4D0E6E48C994FCA4E2ED55F473_9265169-1_l.jpg',1);
+Insert into BOOK (BID,BNAME,PRICE,AUTHOR,IMAGE,CID) values ('7','252',252,'252','book_img/A728D53FB03F48A8B197B4B1837E7604_23878124-1_b_2.jpg',1);
+Insert into BOOK (BID,BNAME,PRICE,AUTHOR,IMAGE,CID) values ('17','spring boot',1520,'111','book_img/928553565FC04BE4BF8E8A1011FC6CAF_26.jpg',1);
+Insert into BOOK (BID,BNAME,PRICE,AUTHOR,IMAGE,CID) values ('4','java1',11,'1','book_img/C9197F3653934D2E93617130ABC1E403_27.jpg',1);
---
+++ bookstore/WebRoot/sql/category_data.sql
@@ -0,0 +1,5 @@
+REM INSERTING into CATEGORY
+Insert into CATEGORY (CID,CNAME) values ('4','java web ¿ò¼Ü');
+Insert into CATEGORY (CID,CNAME) values ('8','´óÊý¾Ý');
+Insert into CATEGORY (CID,CNAME) values ('1','JavaSE');
+Insert into CATEGORY (CID,CNAME) values ('2','JavaEE');
